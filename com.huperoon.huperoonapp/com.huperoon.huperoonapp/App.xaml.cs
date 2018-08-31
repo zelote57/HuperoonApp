9fbd0f6bba5b17f969d435bf303ab120f9cb3624
Gustavo Lozada <aprogramador7@transferunion.com>
2018-08-31 09:10:00 -0500

    first commit

---
+++ com.huperoon.huperoonapp/com.huperoon.huperoonapp/App.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+namespace com.huperoon.huperoonapp
+{
+    public partial class App : Application
+    {
+        #region Constructor
+
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        #endregion
+
+        #region Methods
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+
+        #endregion
+    }
+}
